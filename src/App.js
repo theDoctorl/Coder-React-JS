0f6108c6bdcb93198266df1b5b26efa0159a4af3
Fede <fedeleiva2001@gmail.com>
2022-08-24 11:35:51 -0300

    Empezando con react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let imagen = "escudo-BOCAJRS.png"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={imagen} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boca Juniors
         </p>
         <a
           className="App-link"
