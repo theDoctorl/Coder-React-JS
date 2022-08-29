6164e2e04481c8e4294d52c961d9f79c15e7061d
Fede <fedeleiva2001@gmail.com>
2022-08-29 14:47:12 -0300

    Desafio numero 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-
 import './App.css';
 
+import Header from './components/NavBar';
+
+
 function App() {
-  let imagen = "escudo-BOCAJRS.png"
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={imagen} className="App-logo" alt="logo" />
-        <p>
-          Boca Juniors
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />  
     </div>
   );
 }
