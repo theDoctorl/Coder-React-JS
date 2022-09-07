1151c0842991b6920be87fb524f07b32df77e7c2
Fede <fedeleiva2001@gmail.com>
2022-09-07 10:43:20 -0300

    Desafio promesas terminado

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,11 +5,15 @@ import React from "react";
 
 const Item = ({id, nombre, imagen, precio}) => {
 
+    let styles = {
+        width:200,
+        height:200
+    }
 
     return (
         
             <div className="card">
-                <img src={imagen} className="card-img-top" alt={nombre} />
+                <img src={imagen} className="card-img-top" style={styles} alt={nombre} />
                     <div className="card-body">
                         <h5 className="card-title text-center" >{nombre}</h5>
                         <p className="card-text text-center">${precio}</p>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import imagen from "../Images/CamisetasArgentina/Arg-86.webp";
+import imagen2 from "../Images/CamisetasArgentina/Arg-93.webp";
+import imagen3 from "../Images/CamisetasArgentina/ARG-94.webp";
+import imagen4 from "../Images/CamisetasArgentina/Arg-Sub20.webp";
 import ItemList from "../ItemList/itemList";
 
 
@@ -15,10 +19,10 @@ const ItemListContainer = () =>{
     const [items, setItems] = useState([]);
     useEffect(()=>{
         const productos =[
-            {"id": 1,   "nombre": "Camiseta retro Argentina. Temporada `86", "precio": 10000, "imagen": "../../Images/CamisetasArgentina/Arg-86.webp"},
-            {"id": 2,  "nombre": "Camiseta retro Argentina. Temporada `93", "precio": 9500, "imagen": "../Images/CamisetasArgentina/Arg-93.webp"},
-            {"id": 3,  "nombre": "Camiseta retro Argentina. Temporada `94", "precio": 6300, "imagen": "../Images/CamisetasArgentina/Arg-94.webp"},
-            {"id": 4,  "nombre": "Camiseta retro Argentina. Temporada `Sub 20", "precio": 6300, "imagen": "../Images/CamisetasArgentina/Arg-Sub20.webp"}         
+            {"id": 1,   "nombre": "Camiseta retro Argentina. Temporada `86", "precio": 10000, "imagen": imagen},
+            {"id": 2,  "nombre": "Camiseta retro Argentina. Temporada `93", "precio": 9500, "imagen": imagen2},
+            {"id": 3,  "nombre": "Camiseta retro Argentina. Temporada `94", "precio": 6300, "imagen": imagen3 },
+            {"id": 4,  "nombre": "Camiseta retro Argentina. Temporada `Sub 20", "precio": 6300, "imagen": imagen4 }         
         ];
             const promesa = new Promise((resolve, reject)=>{
                 setTimeout(() => {
