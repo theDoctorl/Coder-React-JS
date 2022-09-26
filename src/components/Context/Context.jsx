ee6b339720476c61956adde15c9366eefdb4f297
Fede <fedeleiva2001@gmail.com>
2022-09-26 18:23:43 -0300

    Cart View

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -2,7 +2,7 @@ import React, { useState, createContext } from "react";
 
 export const CartContext = createContext();
 
-const Provider = (props) => {
+const Provider = ({children}) => {
     const [cart, setCart] = useState([]);
 
     const addItem = (item, cantidad) => {
@@ -15,6 +15,10 @@ const Provider = (props) => {
         }
     }
 
+    const removeItem = (id) => {
+        setCart(cart.filter(item => item.id !== id));
+    }
+
     const clear = () => {
         setCart([]);
     }
@@ -23,13 +27,17 @@ const Provider = (props) => {
         return cart.some(item => item.id === id);
     }
 
-    const cartTotal = () => { 
+    const cartTotal = () => {
         return cart.reduce((total, item) => total+=item.cantidad, 0);
     }
 
+    const cartSuma = () => {
+        return cart.reduce((total, item) => total+=item.cantidad*item.precio, 0);
+    }
+
     return (
-        <CartContext.Provider value={{cart, addItem, clear, cartTotal}}>
-            {props.children}
+        <CartContext.Provider value={{cart, addItem, removeItem, clear, cartTotal, cartSuma}}>
+            {children}
         </CartContext.Provider>
     )
 }
--- src/components/carrito/carrito.jsx
+++ src/components/carrito/carrito.jsx
@@ -1,24 +1,49 @@
-import {React, useContext} from "react";
-import {CartContext} from '../Context/Context';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../Context/Context";
+import cesto from "../Images/trash3.svg";
+import carrito from "../Images/basket.svg";
 
+const Carrito = () => {
+    const {cart, removeItem, clear, cartTotal, cartSuma} = useContext(CartContext);
 
-const Carrito = () =>{
-    const {cart, clear} = useContext(CartContext);
-    return(
-        <div>
-            <h2>Carrito</h2>
-            {cart.map((producto) => (
-                <div key={producto.id}>
-                    <h3>{producto.title}</h3>
-                    <button onClick={() => clear(producto.id)}>
-                        Delete
-                    </button>
-                </div>
-            ))}
-            <button onClick={clear}>Delete all</button>
+    return (
+        <div className="container">
+            {cartTotal() > 0 ?
+                <table className="table">
+                    <tbody>
+                        <tr>
+                            <td colSpan={"5"} className="text-end">
+                                <Link onClick={() => {clear()}}>
+                                    <button className="btn fondo_rojo" title="Vaciar Carrito">Vaciar Carrito <img src={cesto} alt="Vaciar Carrito" width="16" /></button>
+                                </Link>
+                            </td>
+                        </tr>
+                        {cart.map(item => (
+                            <tr key={item.id}>
+                                <td className="text-start"><img src={"images/" + item.imagen} alt={item.nombre} title={item.nombre} width="120" /></td>
+                                <td className="text-start align-middle">{item.nombre}</td>
+                                <td className="text-end align-middle">{item.cantidad} x ${item.precio}</td>
+                                <td className="text-end align-middle">${item.cantidad * item.precio}</td>
+                                <td className="text-end align-middle">
+                                    <Link onClick={() => {removeItem(item.id)}}><img src={cesto} alt="Eliminar Producto" title="Eliminar Producto" width="24" /></Link>
+                                </td>
+                            </tr>
+                        ))}
+                        <tr>
+                            <td colSpan={3} className="text-end fw-bold">Total a Pagar</td>
+                            <td className="text-end fw-bold">${cartSuma()}</td>
+                            <td className="text-end">
+                                <Link to={"/checkout"} title="Finalizar Compra">
+                                    <button className="btn fondo_naranja">Finalizar Compra <img src={carrito} alt="Finalizar Compra" width="16" /></button>
+                                </Link>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            : <div className="alert alert-danger text-center" role="alert">No se encontraron Productos!</div>}
         </div>
-
-    );
-};
+    )
+}
 
 export default Carrito;
